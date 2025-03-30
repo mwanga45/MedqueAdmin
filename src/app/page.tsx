29d79cfd7a829aeabcce6fd903524b0cd2912aa3
Lynxprazo <issamwanga02@gmail.com>
2025-03-30 13:36:41 +0300

    more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-'use-client'
+"use-client";
 import React from "react";
+import "./page.css";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-   <div>
-       < button name="go-to">Get started</button>
-   </div>
+    <div className="container">
+      <div className="go-to">
+        <button name="go-to">Get started</button>
+        <FaArrowRight/>
+      </div>
+    </div>
   );
 }
