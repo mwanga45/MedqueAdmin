c15224b8c0b10989304eb7a175d2d5b52ad6676e
Lynxprazo <issamwanga02@gmail.com>
2025-03-30 21:27:49 +0300

    more changes

--- src/app/authentic/page.tsx
+++ src/app/authentic/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import React from "react";
-
+import Register from "../components/login";
+import { BiHealth } from "react-icons/bi";
+import "./authentic.css";
 export default function Authentic() {
   return (
-    <div className="authenc-container">
-      <div className="left-Acontainer"></div>
-      <div className="right-Acontainer"></div>
+    <div className="authentic-container">
+      <div className="left-Acontainer">
+        <Register />
+      </div>
+      <div className="right-Acontainer">
+        <div className="right-content">
+          <BiHealth />
+          <p className="right-title">Welcome To Medqueue</p>
+          <p>let change our life with it </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-export default function Register (){
-    return(
-        <div></div>
-    )
+export default interface Staff {
+  name: string;
+  registration: number;
+  email: string;
+  phone: number;
+  home_address: string;
+}
+export default function Register() {
+  return (
+  <div className="reg-container">
+
+  </div>
+);
 }
