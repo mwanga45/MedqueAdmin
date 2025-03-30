71e2dc3497c9fbc418c19d289f420bad13a32348
Lynxprazo <issamwanga02@gmail.com>
2025-03-30 21:58:43 +0300

    more changes

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -6,23 +6,23 @@ export default function Login (){
         <div className="reg-container">
             <form>
                 <div>
-                    <label htmlFor="name-input"></label>
+                    <label htmlFor="name-input">Username</label>
                     <input type="text" name="name" id="name-input" placeholder="Enter your name please"/>
                 </div>
                 <div>
-                    <label htmlFor="Email-input"></label>
+                    <label htmlFor="Email-input">Email</label>
                     <input type="email" id="Email-input" placeholder="Enter your name please"/>
                 </div>
                 <div>
-                    <label htmlFor="RegNumber-input"></label>
+                    <label htmlFor="RegNumber-input">Registration Number</label>
                     <input type="text" name="RegNumber" id="RegNumber" placeholder="Enter your name please"/>
                 </div>
                 <div>
-                    <label htmlFor="RegNumber-input"></label>
+                    <label htmlFor="RegNumber-input">Password</label>
                     <input type="password" name="user-password" id="RegNumber" placeholder="Enter your name please"/>
                 </div>
                 <div>
-                    <label htmlFor="RegNumber-input"></label>
+                    <label htmlFor="RegNumber-input">Confirm Password</label>
                     <input type="password" name="password-confirm" id="RegNumber" placeholder="Enter your name please"/>
                 </div>
             </form>
--- src/app/components/registerForm.tsx
+++ src/app/components/registerForm.tsx
@@ -12,24 +12,24 @@ export default function Register() {
     <div className="reg-container">
     <form>
         <div>
-            <label htmlFor="name-input"></label>
+            <label htmlFor="name-input">Username</label>
             <input type="text" name="name" id="name-input" placeholder="Enter your name please"/>
         </div>
         <div>
-            <label htmlFor="Email-input"></label>
-            <input type="email" id="Email-input" placeholder="Enter your name please"/>
+            <label htmlFor="Email-input">Email</label>
+            <input type="email" id="Email-input" placeholder="Enter your Email please"/>
         </div>
         <div>
-            <label htmlFor="RegNumber-input"></label>
-            <input type="text" name="RegNumber" id="RegNumber" placeholder="Enter your name please"/>
+            <label htmlFor="RegNumber-input">Registration Number</label>
+            <input type="text" name="RegNumber" id="RegNumber" placeholder="Enter your Registration number please"/>
         </div>
         <div>
-            <label htmlFor="RegNumber-input"></label>
-            <input type="password" name="user-password" id="RegNumber" placeholder="Enter your name please"/>
+            <label htmlFor="RegNumber-input">Password</label>
+            <input type="password" name="user-password" id="RegNumber" placeholder="Enter your Password please"/>
         </div>
         <div>
-            <label htmlFor="RegNumber-input"></label>
-            <input type="password" name="password-confirm" id="RegNumber" placeholder="Enter your name please"/>
+            <label htmlFor="RegNumber-input">Confirm Password</label>
+            <input type="password" name="password-confirm" id="RegNumber" placeholder="Re type your password to confirm"/>
         </div>
     </form>
 </div>
