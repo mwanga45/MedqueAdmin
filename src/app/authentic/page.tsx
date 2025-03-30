5b645017daf9c3ee9a4a66472390c25d4efa0847
Lynxprazo <issamwanga02@gmail.com>
2025-03-30 21:47:22 +0300

    more changes

--- src/app/authentic/page.tsx
+++ src/app/authentic/page.tsx
@@ -2,12 +2,20 @@
 import React from "react";
 import Register from "../components/login";
 import { BiHealth } from "react-icons/bi";
+import { FaHospitalSymbol } from "react-icons/fa";
+import { GiBigWave } from "react-icons/gi";
 import "./authentic.css";
 export default function Authentic() {
   return (
     <div className="authentic-container">
       <div className="left-Acontainer">
-        <Register />
+        <div className="left-svg">
+          <FaHospitalSymbol />
+          <GiBigWave />
+        </div>
+        <div className="form-register">
+          <Register />
+        </div>
       </div>
       <div className="right-Acontainer">
         <div className="right-content">
--- src/app/components/register.tsx
+++ src/app/components/register.tsx
@@ -1,7 +1,31 @@
+"use client"
 import React from "react";
 
 export default function Login (){
     return(
-        <div></div>
+        <div>
+            <form>
+                <div>
+                    <label htmlFor="name-input"></label>
+                    <input type="text" name="name" id="name-input" placeholder="Enter your name please"/>
+                </div>
+                <div>
+                    <label htmlFor="Email-input"></label>
+                    <input type="email" id="Email-input" placeholder="Enter your name please"/>
+                </div>
+                <div>
+                    <label htmlFor="RegNumber-input"></label>
+                    <input type="text" name="RegNumber" id="RegNumber" placeholder="Enter your name please"/>
+                </div>
+                <div>
+                    <label htmlFor="RegNumber-input"></label>
+                    <input type="password" name="user-password" id="RegNumber" placeholder="Enter your name please"/>
+                </div>
+                <div>
+                    <label htmlFor="RegNumber-input"></label>
+                    <input type="password" name="password-confirm" id="RegNumber" placeholder="Enter your name please"/>
+                </div>
+            </form>
+        </div>
     )
 }
