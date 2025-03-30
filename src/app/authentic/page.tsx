d7e2cc970df3bbc22c0e3c6c7f19d492112a3dc3
Lynxprazo <issamwanga02@gmail.com>
2025-03-30 20:19:43 +0300

    more changes

---
+++ src/app/authentic/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Authentic() {
+  return (
+    <div className="authenc-container">
+      <div className="left-Acontainer"></div>
+      <div className="right-Acontainer"></div>
+    </div>
+  );
+}
