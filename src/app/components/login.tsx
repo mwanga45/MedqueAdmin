3b951caf73f677e02894ac920f018328badb06d4
Lynxprazo <issamwanga02@gmail.com>
2025-03-31 09:53:03 +0300

    more changes

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,26 +1,46 @@
-"use client"
+"use client";
 import React from "react";
 
-export default function Login (){
-    return(
-        <div className="log-container">
-            <div className="logTitle">
-                <p>Sign Here!</p>
-            </div>
-            <form>
-                <div className="log-inputConatiner">
-                    <label htmlFor="name-input">Username</label>
-                    <input type="text" name="name" id="name-input" placeholder="Enter your name please"/>
-                </div>
-                <div className="log-inputConatiner">
-                    <label htmlFor="RegNumber-input">Registration Number</label>
-                    <input type="text" name="RegNumber" id="RegNumber" placeholder="Enter your name please"/>
-                </div>
-                <div className="log-inputConatiner">
-                    <label htmlFor="RegNumber-input">Password</label>
-                    <input type="password" name="user-password" id="RegNumber" placeholder="Enter your name please"/>
-                </div>
-            </form>
+export default function Login() {
+  return (
+    <div className="log-container">
+      <div className="logTitle">
+        <p>Sign Here!</p>
+      </div>
+      <form>
+        <div className="log-inputConatiner">
+          <label htmlFor="name-input">Username</label>
+          <input
+            type="text"
+            name="name"
+            id="name-input"
+            placeholder="Enter your name please"
+          />
         </div>
-    )
+        <div className="log-inputConatiner">
+          <label htmlFor="RegNumber-input">Registration Number</label>
+          <input
+            type="text"
+            name="RegNumber"
+            id="RegNumber"
+            placeholder="Enter your name please"
+          />
+        </div>
+        <div className="log-inputConatiner">
+          <label htmlFor="RegNumber-input">Password</label>
+          <input
+            type="password"
+            name="user-password"
+            id="RegNumber"
+            placeholder="Enter your name please"
+          />
+        </div>
+        <div className="logbtn-container">
+          <button type="submit" name="login" className="log-btn">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
