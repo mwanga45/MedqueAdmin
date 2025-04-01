77eb5d545fbb80e5d4f89ecad8a54608b800e624
Lynxprazo <issamwanga02@gmail.com>
2025-04-01 12:52:58 +0300

    more changes

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -2,10 +2,11 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import { MdCloseFullscreen } from "react-icons/md";
+import { MdDashboard } from "react-icons/md";
 
 export default function Sidebar() {
   return (
-    <div>
+    <div  className='nav-container'>
         <div className="nav-upper">
           <p>Hello world</p>
           <MdCloseFullscreen/>
@@ -13,6 +14,52 @@ export default function Sidebar() {
         <div className="nav-searchbar">
           <input type='text' name='search' placeholder='search'/>
           <FaSearch/>
+        </div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="nav-crossline"></div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="navlink-container">
+          <ul>
+            <li>Dashboard</li>
+            <li><MdDashboard/></li>
+          </ul>
+        </div>
+        <div className="nav-account">
+          
         </div>
     </div>
   )
