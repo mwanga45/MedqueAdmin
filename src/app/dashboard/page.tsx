139aa4a2b76a4eb1ebd1784e6f00f18f64fa901e
Lynxprazo <issamwanga02@gmail.com>
2025-04-01 10:18:18 +0300

    more changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 import React from 'react'
+import "./dashboard.css"
 export default function Dashboard() {
   return (
-    <div>
-      <p>Hello world</p>
+    <div className='dash-container'>
+       <div className="dash-upperboard"></div>
+       <div className="dash-sidebar"></div>
+       <div className="dash-maincontainer"></div>
     </div>
   )
 }
