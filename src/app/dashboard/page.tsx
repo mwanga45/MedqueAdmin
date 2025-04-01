2a27aa9f6aeff94c8b4e6ffef70fcf32f31041dc
Lynxprazo <issamwanga02@gmail.com>
2025-04-01 14:35:45 +0300

    more changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,20 +2,38 @@
 import React from "react";
 import "./dashboard.css";
 import Sidebar from "../components/sidebar";
+import { BiHealth } from "react-icons/bi";
 export default function Dashboard() {
-    const handlesidebar = ()=>{
-    }
+  const handlesidebar = () => {};
   return (
     <div className="dash-container">
       <div className="dash-sidebar">
-        <Sidebar/>
+        <Sidebar />
       </div>
       <div className="dash-upperboard">
-        hELLO
+        <div className="unv-title">
+            <BiHealth size={50} color="green"/>
+            <p style={{color:"green", fontSize:30,fontWeight:900,}}>Medqueue Admin-Site</p>
+        </div>
+        <div className="upper-innercontainer">
+            <p className="page-title">DashBoard</p>
+        </div>
+        <div className="optional-dashcontainer">
+            <div className="optional-dash">
+                <p>Booking</p>
+            </div>
+            <div className="optional-dash">
+                <p>Nurse</p>
+            </div>
+            <div className="optional-dash">
+                <p>Doctor</p>
+            </div>
+            <div className="optional-dash">
+                <p>Booking</p>
+            </div>
+        </div>
       </div>
-      <div className="dash-maincontainer">
-        NOPE
-      </div>
+      <div className="dash-maincontainer">NOPE</div>
     </div>
   );
 }
