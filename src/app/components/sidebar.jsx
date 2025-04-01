939e75da150bb21b088ba6eca86ef1d480c1b7c4
Lynxprazo <issamwanga02@gmail.com>
2025-04-01 12:37:45 +0300

    more changes

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,9 +1,19 @@
+'use client'
 import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { MdCloseFullscreen } from "react-icons/md";
 
 export default function Sidebar() {
   return (
     <div>
-      
+        <div className="nav-upper">
+          <p>Hello world</p>
+          <MdCloseFullscreen/>
+        </div>
+        <div className="nav-searchbar">
+          <input type='text' name='search' placeholder='search'/>
+          <FaSearch/>
+        </div>
     </div>
   )
 }
