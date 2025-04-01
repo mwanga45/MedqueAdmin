14eb4f9bff8253a3e1eb8042fc525b7fc5ec5308
Lynxprazo <issamwanga02@gmail.com>
2025-04-01 16:07:30 +0300

    more changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,34 +15,35 @@ export default function Dashboard() {
       </div>
       <div className="dash-upperboard">
         <div className="unv-title">
-            <BiHealth size={50} color="green"/>
-            <p style={{color:"green", fontSize:30,fontWeight:900,}}>Medqueue Admin-Site</p>
+          <BiHealth size={50} color="green" />
+          <p style={{ color: "green", fontSize: 30, fontWeight: 900 }}>
+            Medqueue Admin-Site
+          </p>
         </div>
         <div className="upper-innercontainer">
-            <p className="page-title">DashBoard</p>
+          <p className="page-title">DashBoard</p>
         </div>
         <div className="optional-dashcontainer">
-            <div className="optional-dash">
-                <p>Booking</p>
-            </div>
-            <div className="optional-dash">
-                <p>Nurse</p>
-            </div>
-            <div className="optional-dash">
-                <p>Doctor</p>
-            </div>
-            {/* <div className="optional-dash">
+          <div className="optional-dash">
+            <p>Booking</p>
+          </div>
+          <div className="optional-dash">
+            <p>Nurse</p>
+          </div>
+          <div className="optional-dash">
+            <p>Doctor</p>
+          </div>
+          {/* <div className="optional-dash">
                 <p>Booking</p>
             </div> */}
         </div>
-         <div className="Upper-searchbar">
-                  <input type='text' name='search' placeholder='search'/>
-                  <FaSearch size={20} color="black"/>
-                </div>
+        <div className="Upper-searchbar">
+          <input type="text" name="search" placeholder="search" />
+          <FaSearch size={20} color="black" />
+        </div>
       </div>
       <div className="dash-maincontainer">
-          <AnimatedTable/>
-
+        <AnimatedTable />
       </div>
     </div>
   );
