e5efe5547021e538a164e6a6848cad86b58be343
mwanga45 <issamwanga02@gmail.com>
2025-07-02 22:17:09 +0300

    update API URL and remove unused CSS styles for navigation links

--- src/app/Apiurl.ts
+++ src/app/Apiurl.ts
@@ -1,2 +1,3 @@
-export const apiurl  = "http://192.168.152.123:8801/"
+export const apiurl  = "http://192.168.198.38:8801/"
+// export const apiurl  = "http://192.168.152.123:8801/"
 // export const apiurl  = "http://192.168.137.1:8801/"
